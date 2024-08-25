faac614d7c266e90aff2bd749a59b77e6aaad66e
Abdul Samad <samadmirza269@gmail.com>
2024-08-26 03:29:17 +0500

    Mobile navbar done

---
+++ src/header.js
@@ -0,0 +1,11 @@
+// function to toggle navbar on mobile devices
+function toggleMobileNavbar() {
+  const navbar = document.querySelector("[data-navbar]");
+  const navToggleBtns = document.querySelectorAll("[data-nav-toggle-btn]");
+
+  navToggleBtns.forEach(navToggler => {
+    navToggler.addEventListener('click',() => navbar.classList.toggle("active"));
+  });
+}
+
+toggleMobileNavbar();
