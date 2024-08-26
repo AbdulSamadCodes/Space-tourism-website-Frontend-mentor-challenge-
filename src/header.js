346f357e56f76d9a72c71f02ebd13a767ac917c4
Abdul Samad <samadmirza269@gmail.com>
2024-08-26 12:42:42 +0500

    Header component completed

--- src/header.js
+++ src/header.js
@@ -1,4 +1,4 @@
-// function to toggle navbar on mobile devices
+//function to toggle navbar on mobile devices
 function toggleMobileNavbar() {
   const navbar = document.querySelector("[data-navbar]");
   const navToggleBtns = document.querySelectorAll("[data-nav-toggle-btn]");
@@ -9,4 +9,21 @@ function toggleMobileNavbar() {
 }
 
 toggleMobileNavbar();
+
+//function to update the navbar outline width dynmaically
+function updateOutlineWidth() {
+  const logo = document.querySelector('[data-logo]');
+  const navbar = document.querySelector('[data-navbar]');
+
+  let width;
+  const {right:logoPosition} = logo.getBoundingClientRect();
+  const {left:navbarPosition} = navbar.getBoundingClientRect();
+
+  width = (navbarPosition - logoPosition) - 50;
+  navbar.style.setProperty('--outline-width',`${width}px`);  
+}
+
+window.addEventListener("resize",updateOutlineWidth);
+window.addEventListener("load",updateOutlineWidth);
+
 
