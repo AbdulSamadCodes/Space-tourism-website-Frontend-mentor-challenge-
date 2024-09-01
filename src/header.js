53c1d6957fcb72a782f636f09119fcaa5e06b6d1
Abdul Samad <samadmirza269@gmail.com>
2024-09-01 19:41:20 +0500

    Home page media queries done

--- src/header.js
+++ src/header.js
@@ -10,6 +10,11 @@ function toggleMobileNavbar() {
 
 toggleMobileNavbar();
 
+//function to get the minimum gap between outline and logo
+function getMinimumGap() {
+  return parseInt(getComputedStyle(document.documentElement).getPropertyValue('--minimum-outline-gap'));
+}
+
 //function to update the navbar outline width dynmaically
 function updateOutlineWidth() {
   const logo = document.querySelector('[data-logo]');
@@ -18,10 +23,12 @@ function updateOutlineWidth() {
   let width;
   const {right:logoPosition} = logo.getBoundingClientRect();
   const {left:navbarPosition} = navbar.getBoundingClientRect();
+  const minimumGap = getMinimumGap();
 
-  width = (navbarPosition - logoPosition) - 40;
+  width = Math.floor((navbarPosition - logoPosition) - minimumGap);
   navbar.style.setProperty('--outline-width',`${width}px`);  
 }
 
 window.addEventListener("resize",updateOutlineWidth);
 window.addEventListener("load",updateOutlineWidth);
+
