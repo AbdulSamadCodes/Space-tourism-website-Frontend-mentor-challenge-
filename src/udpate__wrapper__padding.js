b45cea5544b6996f8a6fa32a41c96b14be60a7b9
Abdul Samad <samadmirza269@gmail.com>
2024-09-01 20:30:22 +0500

    Home page completed

--- src/udpate__wrapper__padding.js
+++ src/udpate__wrapper__padding.js
@@ -1,3 +1,36 @@
+//functionn to compute the wrapper inline padding
+function computeWrapperPadding(wrapper, navbar) {
+  const wrapperLeftPosition = Math.floor(wrapper.getBoundingClientRect().left);
+  
+  const navbarLeftPosition = Math.floor(navbar.getBoundingClientRect().left);
+  const navbarLeftInset = Math.floor(parseInt(getComputedStyle(navbar).getPropertyValue('--left-inset')));
+  const outlineWidth = Math.floor(parseInt(getComputedStyle(navbar).getPropertyValue('--outline-width')))
+  const outlineLeftPosition = Math.floor(navbarLeftPosition - (outlineWidth - navbarLeftInset));
+
+  const wrapperPaddingInline = outlineLeftPosition - wrapperLeftPosition;
+  return wrapperPaddingInline;
+}
+
+//function to update the wrapper padding inline
+function updateWrapperPadding(paddingInline) {
+  document.documentElement.style.setProperty('--wrapper-padding-inline',`${paddingInline}px`);  
+}
+
+function handleWrapperPadding() {
+  const wrapper = document.querySelector('[data-wrapper]');
+  const navbar = document.querySelector('[data-navbar]');
+  
+  const defaultWrapperPaddingInline = 20;
+  const paddingInline = window.innerWidth > 992 ? computeWrapperPadding(wrapper,navbar) : defaultWrapperPaddingInline; 
+
+  updateWrapperPadding(paddingInline);
+}
+
+window.addEventListener("resize",handleWrapperPadding);
+window.addEventListener("load",handleWrapperPadding);
+
+updateWrapperPadding();
+
 
 
 
